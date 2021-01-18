e522dbd865dd99e638700a4ca56021a8d5391a1e
Moritz Obermeier <moritz@comydo.com>
2021-01-18 13:00:55 +0100

    'fixed' build/install scripts

--- scripts/moobsen/build-moobsen.sh
+++ scripts/moobsen/build-moobsen.sh
@@ -2,11 +2,8 @@
 # not pretty but who cares?
 
 # run from git root (linux/)
-<<<<<<< HEAD
-=======
 cd $(git rev-parse --show-toplevel)
->>>>>>> cb36721bc43532c8efe1f040300efb8578deb193
 
 rm -rf ./debian
 rm vmlinux-gdb.py
-make -j 4 deb-pkg LOCALVERSION=-moobsen
+time make -j 16 deb-pkg LOCALVERSION=-moobsen
--- scripts/moobsen/install-moobsen.sh
+++ scripts/moobsen/install-moobsen.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
 
-cd ../../
-=======
 cd $(git rev-parse --show-toplevel)
 cd ../
->>>>>>> cb36721bc43532c8efe1f040300efb8578deb193
 sudo dpkg -i linux-*.deb
 mv linux-*-moobsen* ./linux_build/
